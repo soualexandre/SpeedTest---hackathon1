b7433d5e219e0fb89d05d6a267bfb0cf9e322540
Alexandre Souza dos Santos <55295879+soualexandre@users.noreply.github.com>
2019-09-27 17:27:34 -0300

    Add files via upload

---
+++ view/cadastrar.php
@@ -0,0 +1,92 @@
+<html lang="pt-br">
+<head>
+	<title>GreenFarm.br</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="../login-obj/vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="../login-obj/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="../login-obj/fonts/iconic/css/material-design-iconic-font.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="../login-obj/vendor/animate/animate.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="../login-obj/vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="../login-obj/vendor/animsition/css/animsition.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="../login-obj/vendor/select2/select2.min.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="../login-obj/vendor/daterangepicker/daterangepicker.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="../login-obj/css/util.css">
+	<link rel="stylesheet" type="text/css" href="../login-obj/css/main.css">
+<!--===============================================================================================-->
+</head>
+<body>
+	
+	<div class="limiter">
+		<div class="container-login100">
+			<div class="wrap-login100 p-t-85 p-b-20">
+				<form class="login100-form validate-form">
+					<span class="login100-form-title p-b-70">
+						Greenfarm.br
+					</span>
+				
+
+					<div class="wrap-input100 validate-input m-t-85 m-b-35" data-validate = "Inrisa o Email">
+						<input class="input100" type="email" name="email">
+						<span class="focus-input100" data-placeholder="email"></span>
+					</div>
+						<div class="wrap-input100 validate-input m-t-85 m-b-35" data-validate = "nome">
+						<input class="input100" type="text" name="nome">
+						<span class="focus-input100" data-placeholder="Nome completo"></span>
+					</div>
+						<div class="wrap-input100 validate-input m-t-85 m-b-35" data-validate = "nome">
+						<input class="input100" type="text" name="nome">
+						<span class="focus-input100" data-placeholder="Nome completo"></span>
+					</div>
+						<div class="wrap-input100 validate-input m-t-85 m-b-35" data-validate = "nome">
+						<input class="input100" type="text" name="nome">
+						<span class="focus-input100" data-placeholder="Nome completo"></span>
+					</div>
+					<div class="wrap-input100 validate-input m-b-50" data-validate="Enter password">
+						<input class="input100" type="password" name="pass">
+						<span class="focus-input100" data-placeholder="Password"></span>
+					</div>
+
+					<div class="container-login100-form-btn">
+						<button class="login100-form-btn">
+							Criar conta
+						</button>
+					</div>
+
+					
+				</form>
+			</div>
+		</div>
+	</div>
+	
+
+	<div id="dropDownSelect1"></div>
+	
+<!--===============================================================================================-->
+	<script src="../login-obj/vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script src="../login-obj/vendor/animsition/js/animsition.min.js"></script>
+<!--===============================================================================================-->
+	<script src="../login-obj/vendor/bootstrap/js/popper.js"></script>
+	<script src="../login-obj/vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script src="../login-obj/vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+	<script src="../login-obj/vendor/daterangepicker/moment.min.js"></script>
+	<script src="../login-obj/vendor/daterangepicker/daterangepicker.js"></script>
+<!--===============================================================================================-->
+	<script src="../login-obj/vendor/countdowntime/countdowntime.js"></script>
+<!--===============================================================================================-->
+	<script src="../login-obj/js/main.js"></script>
+
+</body>
+</html>
